c67a2bc8b394c5b87db9bd9781116f348700c136
mattl <mattl@LAPTOP-E70R6J6J>
2025-02-01 12:03:18 +0100

    Phase 13 : gestion des sons.

--- src/vue/ChoixJoueur.java
+++ src/vue/ChoixJoueur.java
@@ -14,21 +14,19 @@ import javax.swing.SwingConstants;
 
 import controleur.Controle;
 import controleur.Global;
+import outilspackage.Son;
 
 import java.awt.Cursor;
 import java.awt.Dimension;
 
 /**
  * Frame du choix du joueur
+ * 
  * @author emds
  *
  */
-public class ChoixJoueur extends JFrame {
+public class ChoixJoueur extends JFrame implements Global {
 
-	/**
-	 * Nombre de personnages différents
-	 */
-	private static final int NBPERSOS = 3;
 	/**
 	 * Panel général
 	 */
@@ -49,42 +47,49 @@ public class ChoixJoueur extends JFrame {
 	 * Numéro du personnage sélectionné
 	 */
 	private int numPerso;
+	private Son welcome;
+	private Son precedent;
+	private Son suivant;
+	private Son go;
 
 	/**
 	 * Clic sur la flèche "précédent" pour afficher le personnage précédent
 	 */
 	private void lblPrecedent_clic() {
-		numPerso = ((numPerso+1)%NBPERSOS)+1;
+		numPerso = ((numPerso + 1) % NBPERSOS) + 1;
 		affichePerso();
+		precedent.play();
 	}
-	
+
 	/**
 	 * Clic sur la flèche "suivant" pour afficher le personnage suivant
 	 */
 	private void lblSuivant_clic() {
-		numPerso = (numPerso%NBPERSOS)+1 ;
+		numPerso = (numPerso % NBPERSOS) + 1;
 		affichePerso();
+		suivant.play();
 	}
-	
+
 	/**
 	 * Clic sur GO pour envoyer les informations
 	 */
 	private void lblGo_clic() {
-		if(this.txtPseudo.getText().equals("")) {
+		if (this.txtPseudo.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "La saisie du pseudo est obligatoire");
 			this.txtPseudo.requestFocus();
 		} else {
 			this.controle.evenementChoixJoueur(this.txtPseudo.getText(), numPerso);
+			go.play();
 		}
 	}
-	
+
 	/**
 	 * Affichage du personnage correspondant au numéro numPerso
 	 */
 	private void affichePerso() {
-		String chemin = "personnages/perso"+this.numPerso+"marche"+1+"d"+1+".gif";
+		String chemin = CHEMINPERSONNAGES + PERSO + this.numPerso + MARCHE + 1 + "d" + 1 + EXTFICHIERPERSO;
 		URL resource = getClass().getClassLoader().getResource(chemin);
-		this.lblPersonnage.setIcon(new ImageIcon(resource));		
+		this.lblPersonnage.setIcon(new ImageIcon(resource));
 	}
 
 	/**
@@ -93,7 +98,7 @@ public class ChoixJoueur extends JFrame {
 	private void sourisNormale() {
 		contentPane.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 	}
-	
+
 	/**
 	 * Change le curseur de la souris en forme de doigt pointé
 	 */
@@ -103,15 +108,16 @@ public class ChoixJoueur extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * 
 	 * @param controle instance du contrôleur
 	 */
 	public ChoixJoueur(Controle controle) {
 		// Dimension de la frame en fonction de son contenu
 		this.getContentPane().setPreferredSize(new Dimension(400, 275));
-	    this.pack();
-	    // interdiction de changer la taille
+		this.pack();
+		// interdiction de changer la taille
 		this.setResizable(false);
-		 
+
 		setTitle("Choice");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
@@ -122,80 +128,92 @@ public class ChoixJoueur extends JFrame {
 		lblPersonnage.setBounds(142, 115, 120, 120);
 		lblPersonnage.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblPersonnage);
-		
+
 		JLabel lblPrecedent = new JLabel("");
 		lblPrecedent.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				lblPrecedent_clic();
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				sourisDoigt();
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				sourisNormale();
 			}
 		});
-		
+
 		JLabel lblSuivant = new JLabel("");
 		lblSuivant.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				lblSuivant_clic();
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				sourisDoigt();
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				sourisNormale();
 			}
 		});
-		
+
 		JLabel lblGo = new JLabel("");
 		lblGo.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				lblGo_clic();
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				sourisDoigt();
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				sourisNormale();
 			}
 		});
-		
+
 		txtPseudo = new JTextField();
 		txtPseudo.setBounds(142, 245, 120, 20);
 		contentPane.add(txtPseudo);
 		txtPseudo.setColumns(10);
-		
+
 		lblGo.setBounds(311, 202, 65, 61);
 		contentPane.add(lblGo);
 		lblSuivant.setBounds(301, 145, 25, 46);
 		contentPane.add(lblSuivant);
 		lblPrecedent.setBounds(65, 146, 31, 45);
 		contentPane.add(lblPrecedent);
-		
+
 		JLabel lblFond = new JLabel("");
 		lblFond.setBounds(0, 0, 400, 275);
-		String chemin = "fonds/fondchoix.jpg";
-		URL resource = getClass().getClassLoader().getResource(chemin);
-		lblFond.setIcon(new ImageIcon(resource));		
+		URL resource = getClass().getClassLoader().getResource(FONDCHOIX);
+		lblFond.setIcon(new ImageIcon(resource));
 		contentPane.add(lblFond);
-		
+
 		// récupération de l'instance de Controle
 		this.controle = controle;
-		
+
 		// affichage du premier personnage
 		this.numPerso = 1;
 		this.affichePerso();
+
+		// récupération des sons
+		precedent = new Son(getClass().getClassLoader().getResource(SONPRECEDENT));
+		suivant = new Son(getClass().getClassLoader().getResource(SONSUIVANT));
+		go = new Son(getClass().getClassLoader().getResource(SONGO));
+		welcome = new Son(getClass().getClassLoader().getResource(SONWELCOME));
+		welcome.play();
 
 		// positionnement sur la zone de saisie
 		txtPseudo.requestFocus();
